2498203b2e65e442329e9f2fafef9a612a9e9f49
Shubham <shubhambhardwaj112277@gmail.com>
2024-07-13 22:50:05 +0530

    Day 1 of javascript challenge

---
+++ 01_day_of_js_challenge.js
@@ -0,0 +1,40 @@
+//Activity 1
+var one = 1;
+let two = 2;
+
+console.log("value of one: " + one);
+console.log("value of two: " + two);
+
+//Activity 2
+const flag = true;
+console.log("flag :" + flag);
+
+//Activity 3
+let num = 5;
+
+let str = "hello";
+
+const isStudent = true;
+
+const student = {
+  name : "Shubham",
+  dept : "BCA",
+};
+
+let arr = [1,2,3];
+
+console.log("Type of num : " + typeof(num));
+console.log("Type of str : " + typeof(str));
+console.log("Type of str : " + typeof(isStudent));
+console.log("Type of student : " + typeof(student));
+console.log("Type of arr : " + typeof(arr));
+
+//Activity 4
+let rollNo = 1;
+console.log("rollNo : " + rollNo);
+rollNo = 2;
+console.log("rollNo : " + rollNo);
+
+//Activity 5
+const id = 101;
+id = 102;
